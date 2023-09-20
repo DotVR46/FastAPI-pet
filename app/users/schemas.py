dabe540d4065c7296b243ceca6cfb2a6f7801bf0
DotVR46 <maggot8950@gmail.com>
2023-09-21 01:09:27 +0600

    Проба crud, уборка лишнего

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,7 +1,8 @@
+from annotated_types import MinLen, MaxLen
 from pydantic import BaseModel, EmailStr
-
+from typing import Annotated
 
 class CreateUser(BaseModel):
     id: int
-    username: str
+    username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
--- app/users/views.py
+++ app/users/views.py
@@ -2,31 +2,12 @@ from typing import Annotated
 
 from fastapi import APIRouter, Path, Depends
 
+from app.users import crud
 from app.users.schemas import CreateUser
 
-users_router = APIRouter()
-
-users = [
-    {"id": 1, "username": "user1", "email": "user1@gmail.com"},
-    {"id": 2, "username": "user2", "email": "user2@gmail.com"},
-    {"id": 3, "username": "user3", "email": "user3@gmail.com"},
-]
+router = APIRouter()
 
 
-@users_router.get("/")
-async def get_users_list():
-    return {"users": users}
-
-
-@users_router.get("/{user_id}")
-async def get_user(user_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
-    for user in users:
-        if user["id"] == user_id:
-            return {"user": user}
-    return {"message": "User not found."}
-
-
-@users_router.post("/create")
+@router.post("/create")
 async def create_user(user: CreateUser):
-    users.append(dict(user))
-    return {"message": "User created", "user": user}
+    return crud.create_user(user_in=user)
